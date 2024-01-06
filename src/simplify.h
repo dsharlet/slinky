d3a42012ec6d98674c89da00546f6a7f830ca5b3
Dillon <dsharlet@gmail.com>
2024-01-06 01:33:39 -0800

    Minor regrouping of declarations

--- src/simplify.h
+++ src/simplify.h
@@ -13,6 +13,20 @@ expr simplify(const expr& e, const bounds_map& bounds = bounds_map());
 stmt simplify(const stmt& s, const bounds_map& bounds = bounds_map());
 interval_expr simplify(const interval_expr& e, const bounds_map& bounds = bounds_map());
 
+// Determine an interval such that e is always inside the interval.
+interval_expr bounds_of(const expr& x, const bounds_map& bounds = bounds_map());
+
+// Attempts to determine if e can be proven to be always true or false.
+std::optional<bool> attempt_to_prove(const expr& condition, const bounds_map& bounds = bounds_map());
+bool prove_true(const expr& condition, const bounds_map& bounds = bounds_map());
+bool prove_false(const expr& condition, const bounds_map& bounds = bounds_map());
+
+// Find the interval for `var` that makes `e` true.
+interval_expr where_true(const expr& condition, symbol_id var);
+
+// Compute the derivative of f with respect to x.
+expr differentiate(const expr& f, symbol_id x);
+
 // Helpers for producing simplified versions of ops. These do not recursively simplify their
 // operands. `op` is an existing node that may be returned if op is equivalent. `op` may be null.
 expr simplify(const class min* op, expr a, expr b);
@@ -49,20 +63,6 @@ interval_expr bounds_of(const logical_or* op, interval_expr a, interval_expr b);
 interval_expr bounds_of(const logical_not* op, interval_expr x);
 interval_expr bounds_of(const class select* op, interval_expr c, interval_expr t, interval_expr f);
 interval_expr bounds_of(const call* op, std::vector<interval_expr> args);
-
-// Determine an interval such that e is always inside the interval.
-interval_expr bounds_of(const expr& x, const bounds_map& bounds = bounds_map());
-
-// Attempts to determine if e can be proven to be always true or false.
-std::optional<bool> attempt_to_prove(const expr& condition, const bounds_map& bounds = bounds_map());
-bool prove_true(const expr& condition, const bounds_map& bounds = bounds_map());
-bool prove_false(const expr& condition, const bounds_map& bounds = bounds_map());
-
-// Find the interval for `var` that makes `e` true.
-interval_expr where_true(const expr& condition, symbol_id var);
-
-// Compute the derivative of f with respect to x.
-expr differentiate(const expr& f, symbol_id x);
 
 }  // namespace slinky
 
