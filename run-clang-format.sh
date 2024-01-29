9dee8e3d49f3f0c1502e4d111f93157c1ad2b1d6
Steven Johnson <srj@google.com>
2024-01-29 13:39:06 -0800

    Update run-clang-format.sh

--- run-clang-format.sh
+++ run-clang-format.sh
@@ -5,9 +5,7 @@ set -e
 ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # We are currently standardized on using LLVM/Clang17 for this script.
-# Note that this is totally independent of the version of LLVM that you
-# are using to build Halide itself. If you don't have LLVM17 installed,
-# you can usually install what you need easily via:
+# If you don't have LLVM17 installed, you can usually install what you need easily via:
 #
 # sudo apt-get install llvm-17 clang-17 libclang-17-dev clang-tidy-17
 # export CLANG_FORMAT_LLVM_INSTALL_DIR=/usr/lib/llvm-17
