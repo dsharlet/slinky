f318cd70a22bb35e8aecc750703fca0ea88d3bff
Dillon <dsharlet@gmail.com>
2024-02-07 00:01:53 -0800

    Add benchmark of hardcoded equivalent for comparison

--- runtime/buffer_benchmark.cc
+++ runtime/buffer_benchmark.cc
@@ -13,7 +13,7 @@ __attribute__((noinline)) void memset_slice(void* base, index_t extent) { memset
 template <typename Fn>
 void BM_for_each_contiguous_slice(benchmark::State& state, Fn fn) {
   std::vector<index_t> extents = {state.range(0) + 16, state.range(1), state.range(2)};
-  while (extents.back() == 0) {
+  while (extents.back() == 1) {
     extents.pop_back();
   }
   buffer<char, 3> buf(extents);
@@ -25,14 +25,37 @@ void BM_for_each_contiguous_slice(benchmark::State& state, Fn fn) {
   }
 }
 
+template <typename Fn>
+void BM_for_each_slice_hardcoded(benchmark::State& state, Fn fn) {
+  std::vector<index_t> extents = {state.range(0) + 16, state.range(1), state.range(2)};
+  buffer<char, 3> buf(extents);
+  buf.allocate();
+  buf.dim(0).set_extent(state.range(0));
+
+  for (auto _ : state) {
+    char* base_i = buf.base();
+    for (index_t i = 0; i < buf.dim(2).extent(); ++i) {
+      char* base_j = base_i;
+      for (index_t j = 0; j < buf.dim(1).extent(); ++j) {
+        fn(base_j, buf.dim(0).extent());
+        base_j += buf.dim(1).stride();
+      }
+      base_i += buf.dim(2).stride();
+    }
+  }
+}
+
 // The difference between these two benchmarks on the same size buffer gives an indication of how much time is spent in
 // overhead inside for_each_contiguous_slice.
 void BM_for_each_contiguous_slice_no_op(benchmark::State& state) { BM_for_each_contiguous_slice(state, no_op_slice); }
 void BM_for_each_contiguous_slice_memset(benchmark::State& state) { BM_for_each_contiguous_slice(state, memset_slice); }
+void BM_for_each_slice_hardcoded_memset(benchmark::State& state) { BM_for_each_slice_hardcoded(state, memset_slice); }
 
-BENCHMARK(BM_for_each_contiguous_slice_no_op)->Args({1024, 16, 0});
-BENCHMARK(BM_for_each_contiguous_slice_memset)->Args({1024, 16, 0});
+BENCHMARK(BM_for_each_contiguous_slice_no_op)->Args({1024, 16, 1});
+BENCHMARK(BM_for_each_contiguous_slice_memset)->Args({1024, 16, 1});
+BENCHMARK(BM_for_each_slice_hardcoded_memset)->Args({1024, 16, 1});
 BENCHMARK(BM_for_each_contiguous_slice_no_op)->Args({1024, 4, 4});
 BENCHMARK(BM_for_each_contiguous_slice_memset)->Args({1024, 4, 4});
+BENCHMARK(BM_for_each_slice_hardcoded_memset)->Args({1024, 4, 4});
 
 }  // namespace slinky
