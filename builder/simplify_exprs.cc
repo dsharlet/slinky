8c830b4e39278e899fa61c185bb348085da71324
Dillon <dsharlet@gmail.com>
2024-02-27 19:04:06 -0800

    Fix rule for < -> <=

--- builder/simplify_exprs.cc
+++ builder/simplify_exprs.cc
@@ -432,7 +432,7 @@ expr simplify(const less* op, expr a, expr b) {
       r.rewrite(c0 < x + c1, eval(c0 - c1) < x) ||
 
       r.rewrite((x + c0) / c1 < x / c1, eval(c0 < 0)) ||
-      r.rewrite(x / c1 < (x + c0) / c1, eval(0 < c0)) ||
+      r.rewrite(x / c1 < (x + c0) / c1, eval(c1 <= c0)) ||
     
       r.rewrite(x < x + y, 0 < y) ||
       r.rewrite(x + y < x, y < 0) ||
