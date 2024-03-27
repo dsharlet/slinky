b3b911bc00c138d52022e0218bafc8beeccac258
Dillon <dsharlet@gmail.com>
2024-03-27 03:01:00 -0700

    Add comment

--- runtime/thread_pool.h
+++ runtime/thread_pool.h
@@ -35,7 +35,8 @@ public:
 
   int thread_count() const { return workers_.size(); }
 
-  // Enqueues `n` copies of task `t` on the thread pool queue.
+  // Enqueues `n` copies of task `t` on the thread pool queue. This guarantees that `t` will not
+  // be run recursively on the same thread while in `wait_for`.
   void enqueue(int n, const task& t);
   void enqueue(task t);
   // Waits for `condition` to become true. While waiting, executes tasks on the queue.
