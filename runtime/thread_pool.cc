8914da4df421fe5fcd3d06d97abcab894abfa168
Dillon <dsharlet@gmail.com>
2024-03-26 21:18:09 -0700

    Don't let enqueue run the same task on the same thread more than once recursively

--- runtime/thread_pool.cc
+++ runtime/thread_pool.cc
@@ -23,29 +23,40 @@ thread_pool::~thread_pool() {
   }
 }
 
-thread_pool::task thread_pool::dequeue() {
-  auto& next = task_queue_.front();
-  if (next.first == 1) {
-    task t = std::move(next.second);
-    task_queue_.pop_front();
-    return t;
-  } else {
-    assert(next.first > 1);
-    next.first -= 1;
-    return next.second;
+bool thread_pool::dequeue(task& t, std::vector<thread_pool::task_id>& task_stack) {
+  for (auto i = task_queue_.begin(); i != task_queue_.end(); ++i) {
+    if (std::find(task_stack.begin(), task_stack.end(), std::get<2>(*i)) != task_stack.end()) {
+      // Don't enqueue the same task multiple times on the same thread.
+      continue;
+    }
+    if (std::get<0>(*i) == 1) {
+      t = std::move(std::get<1>(*i));
+      task_queue_.erase(i);
+      task_stack.push_back(0);
+      return true;
+    } else {
+      assert(std::get<0>(*i) > 1);
+      std::get<0>(*i) -= 1;
+      task_stack.push_back(std::get<2>(*i));
+      t = std::get<1>(*i);
+      return true;
+    }
   }
+  return false;
 }
 
 void thread_pool::wait_for(std::function<bool()> condition) {
+  thread_local std::vector<std::size_t> task_stack;
   std::unique_lock l(mutex_);
   while (!condition()) {
-    if (!task_queue_.empty()) {
-      task t = dequeue();
+    task t;
+    if (dequeue(t, task_stack)) {
       l.unlock();
-      task();
+      t();
+      task_stack.pop_back();
       l.lock();
-      // This is pretty inefficient. It is here to wake up threads that are waiting for a condition to become true, that
-      // may have become true due to the task completing. There may be a better way to do this.
+      // This is pretty inefficient. It is here to wake up threads that are waiting for a condition to become true,
+      // that may have become true due to the task completing. There may be a better way to do this.
       cv_.notify_all();
     } else if (!stop_) {
       cv_.wait(l);
@@ -54,14 +65,17 @@ void thread_pool::wait_for(std::function<bool()> condition) {
 }
 
 void thread_pool::enqueue(int n, const task& t) {
+  if (n <= 0) return;
   std::unique_lock l(mutex_);
-  task_queue_.push_back({n, t});
+  task_id id = next_task_id_++;
+  task_queue_.push_back({n, t, id});
   cv_.notify_all();
 }
 
 void thread_pool::enqueue(task t) {
   std::unique_lock l(mutex_);
-  task_queue_.push_back({1, std::move(t)});
+  task_id id = next_task_id_++;
+  task_queue_.push_back({1, std::move(t), id});
   cv_.notify_one();
 }
 
