e7dd9186f96a10e498591a7262ddcb867f5b0247
Dillon <dsharlet@gmail.com>
2024-03-26 21:32:56 -0700

    Minor cleanups

--- builder/slide_and_fold_storage.h
+++ builder/slide_and_fold_storage.h
@@ -1,7 +1,5 @@
-#ifndef SLINKY_BUILDER_INFER_BOUNDS_H
-#define SLINKY_BUILDER_INFER_BOUNDS_H
-
-#include <vector>
+#ifndef SLINKY_BUILDER_SLIDE_AND_FOLD_STORAGE_H
+#define SLINKY_BUILDER_SLIDE_AND_FOLD_STORAGE_H
 
 #include "runtime/stmt.h"
 
@@ -11,4 +9,4 @@ stmt slide_and_fold_storage(const stmt& s, node_context& ctx);
 
 }  // namespace slinky
 
-#endif  // SLINKY_BUILDER_INFER_BOUNDS_H
+#endif  // SLINKY_BUILDER_SLIDE_AND_FOLD_STORAGE_H
