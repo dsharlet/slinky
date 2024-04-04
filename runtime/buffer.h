cd6a884ac0a4a27e6d84c0c2b1d31cf65490d440
Dillon <dsharlet@gmail.com>
2024-04-04 12:20:05 -0700

    Minor refactor

--- runtime/buffer.h
+++ runtime/buffer.h
@@ -634,12 +634,7 @@ void for_each_slice(std::size_t slice_rank, const raw_buffer& buf, const F& f, c
   // I'm not sure if fixing this would be much of an improvement.
   sliced_bufs = {buf, bufs...};
   for (std::size_t i = 0; i < BufsSize; ++i) {
-    if (sliced_bufs[i].rank > buf.rank) {
-      index_t extra_dims = sliced_bufs[i].rank - buf.rank;
-      sliced_bufs[i].rank = slice_rank + extra_dims;
-    } else {
-      sliced_bufs[i].rank = slice_rank;
-    }
+    sliced_bufs[i].rank = slice_rank + std::max(sliced_bufs[i].rank, buf.rank) - buf.rank;
   }
 
   internal::for_each_slice_impl(bases, slice_dims, dims, [&](const std::array<void*, BufsSize>& bases) {
