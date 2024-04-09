090cd80a0596d6a2ad017b37c4b4b81b7714b03e
Volodymyr Kysenko <vksnk@google.com>
2024-04-09 21:45:40 +0000

    Address comments

--- builder/test/pipeline.cc
+++ builder/test/pipeline.cc
@@ -584,12 +584,12 @@ TEST_P(pyramid, pipeline) {
   test_context eval_ctx;
   p.evaluate(inputs, outputs, eval_ctx);
 
+  const int parallel_extra = max_workers != loop::serial ? 1 : 0;
+  ASSERT_EQ(eval_ctx.heap.total_size, (W + 2) / 2 * (2 + parallel_extra) * sizeof(int));
   ASSERT_EQ(eval_ctx.heap.total_count, 1);
+
   if (max_workers == loop::serial) {
-    ASSERT_EQ(eval_ctx.heap.total_size, (W + 2) / 2 * 2 * sizeof(int));
     check_replica_pipeline(define_replica_pipeline(ctx, {in}, {out}));
-  } else {
-    ASSERT_EQ(eval_ctx.heap.total_size, (W + 2) / 2 * 3 * sizeof(int));
   }
 }
 
