dsharlet/slinky

commit fd0b84a92101e662ad42328ac31c7547ca0da071

Speed up simplify a lot by avoiding generating bounds of points (#230)
This was a bit of a rabbit hole:
- Discovered that simplify can be *much* faster if we avoid generating
unnecessary intervals when we already have points.
- This tightened bounds of mod (and probably other things too), which
made pyramid storage folding fail.
- The fix is to track bounds of exprs and use them in storage folding.
This made some interesting changes (tightening the bounds of
parallel_stencils storage folding). They look like a regression, but
they're tighter expressions (probably pointlessly so in this case).

@vksnk *might* be relevant for #225 storage folding issues.

builder/slide_and_fold_storage.cc
@@ -107,6 +107,7 @@ public:
     expr step;
     int max_workers;
     std::unique_ptr<symbol_map<box_expr>> buffer_bounds;
+    std::unique_ptr<symbol_map<interval_expr>> expr_bounds;
 
     // The next few fields relate to implementing synchronization in pipelined loops. In a pipelined loop, we
     // treat a sequence of stmts as "stages" in the pipeline, where we add synchronization to cause the loop
@@ -137,7 +138,8 @@ public:
 
     loop_info(node_context& ctx, var sym, expr orig_min, interval_expr bounds, expr step, int max_workers)
         : sym(sym), orig_min(orig_min), bounds(bounds), step(step), max_workers(max_workers),
-          buffer_bounds(std::make_unique<symbol_map<box_expr>>()), semaphores(ctx, ctx.name(sym) + "_semaphores"),
+          buffer_bounds(std::make_unique<symbol_map<box_expr>>()),
+          expr_bounds(std::make_unique<symbol_map<interval_expr>>()), semaphores(ctx, ctx.name(sym) + "_semaphores"),
           worker_count(ctx, ctx.name(sym) + "_worker_count") {}
   };
   std::vector<loop_info> loops;
@@ -146,6 +148,7 @@ public:
   var x;
 
   symbol_map<box_expr>& current_buffer_bounds() { return *loops.back().buffer_bounds; }
+  symbol_map<interval_expr>& current_expr_bounds() { return *loops.back().expr_bounds; }
 
   slide_and_fold(node_context& ctx) : ctx(ctx), x(ctx.insert_unique("_x")) {
     loops.emplace_back(ctx, var(), expr(), interval_expr::none(), expr(), loop::serial);
@@ -170,6 +173,21 @@ public:
     }
 
     return result;
+  }
+
+  void visit(const let_stmt* op) override { 
+    auto& bounds = current_expr_bounds();
+    std::vector<scoped_value_in_symbol_map<interval_expr>> values;
+    values.reserve(op->lets.size());
+    for (const auto& i : op->lets) {
+      values.push_back(set_value_in_scope(bounds, i.first, bounds_of(i.second, bounds)));
+    }
+    stmt body = mutate(op->body);
+    if (!body.same_as(op->body)) {
+      set_result(let_stmt::make(op->lets, std::move(body)));
+    } else {
+      set_result(op);
+    }
   }
 
   void visit(const allocate* op) override {
@@ -241,7 +259,7 @@ public:
           if (prove_true(ignore_loop_max(overlap.empty()))) {
             // The bounds of each loop iteration do not overlap. We can't re-use work between loop iterations, but we
             // can fold the storage.
-            expr fold_factor = simplify(bounds_of(ignore_loop_max(cur_bounds_d.extent())).max);
+            expr fold_factor = simplify(bounds_of(ignore_loop_max(cur_bounds_d.extent()), *loop.expr_bounds).max);
             if (!depends_on(fold_factor, loop.sym).any()) {
               if (loop.add_synchronization()) {
                 // We need a fold per worker when parallelizing the loop.
@@ -277,15 +295,16 @@ public:
             bool synchronize = loop.add_synchronization();
 
             if (!did_overlapped_fold) {
-              expr fold_factor = simplify(bounds_of(ignore_loop_max(cur_bounds_d.extent())).max);
+              expr fold_factor = simplify(bounds_of(ignore_loop_max(cur_bounds_d.extent()), *loop.expr_bounds).max);
               if (!depends_on(fold_factor, loop.sym).any()) {
                 if (synchronize) {
                   // We need an extra fold per worker when parallelizing the loop.
                   // TODO: This extra folding seems excessive, it allows all workers to execute any stage.
                   // If we can figure out how to add some synchronization to limit the number of workers that
                   // work on a single stage at a time, we should be able to reduce this extra folding.
-                  fold_factor += (loop.worker_count - 1) *
-                                 simplify(bounds_of(ignore_loop_max(cur_bounds_d.max - new_min + 1)).max);
+                  fold_factor +=
+                      (loop.worker_count - 1) *
+                      simplify(bounds_of(ignore_loop_max(cur_bounds_d.max - new_min + 1), *loop.expr_bounds).max);
                 }
                 // Align the fold factor to the loop step size, so it doesn't try to crop across a folding boundary.
                 vector_at(fold_factors[output], d) = simplify(align_up(fold_factor, loop.step));
@@ -404,10 +423,16 @@ public:
     var orig_min(ctx, ctx.name(op->sym) + ".min_orig");
 
     symbol_map<box_expr> last_buffer_bounds = current_buffer_bounds();
+    symbol_map<interval_expr> last_expr_bounds = current_expr_bounds();
     loops.emplace_back(ctx, op->sym, orig_min, bounds(orig_min, op->bounds.max), op->step, op->max_workers);
     current_buffer_bounds() = last_buffer_bounds;
+    current_expr_bounds() = last_expr_bounds;
 
-    stmt body = mutate(op->body);
+    stmt body;
+    {
+      auto set_expr_bounds = set_value_in_scope(current_expr_bounds(), op->sym, op->bounds);
+      body = mutate(op->body);
+    }
     interval_expr loop_bounds = {loops.back().bounds.min, op->bounds.max};
 
     if (loop_bounds.min.same_as(orig_min)) {

builder/test/visualize/parallel_stencils_1.html
@@ -284,7 +284,7 @@ function pipeline(in1, in2, out) {
   check((((buffer_max(out, 1) - buffer_min(out, 1)) + 5) <= buffer_fold_factor(in2, 1)));
   { let intm4 = allocate('intm4', 2, [
       {bounds:{min:buffer_min(out, 0), max:buffer_max(out, 0)}, stride:2, fold_factor:9223372036854775807},
-      {bounds:{min:buffer_min(out, 1), max:buffer_max(out, 1)}, stride:(((buffer_max(out, 0) - buffer_min(out, 0)) * 2) + 2), fold_factor:2}
+      {bounds:{min:buffer_min(out, 1), max:buffer_max(out, 1)}, stride:(((buffer_max(out, 0) - buffer_min(out, 0)) * 2) + 2), fold_factor:(min((buffer_max(out, 1) - buffer_min(out, 1)), 1) + 1)}
     ]);
     { let intm4_uncropped = clone_buffer(intm4);
       { let intm2 = allocate('intm2', 2, [
@@ -294,7 +294,7 @@ function pipeline(in1, in2, out) {
         { let intm2_uncropped = clone_buffer(intm2);
           { let intm3 = allocate('intm3', 2, [
               {bounds:{min:buffer_min(out, 0), max:buffer_max(out, 0)}, stride:2, fold_factor:9223372036854775807},
-              {bounds:{min:buffer_min(out, 1), max:buffer_max(out, 1)}, stride:(((buffer_max(out, 0) - buffer_min(out, 0)) * 2) + 2), fold_factor:2}
+              {bounds:{min:buffer_min(out, 1), max:buffer_max(out, 1)}, stride:(((buffer_max(out, 0) - buffer_min(out, 0)) * 2) + 2), fold_factor:((buffer_max(out, 1) - buffer_min(out, 1)) + 1)}
             ]);
             { let intm3_uncropped = clone_buffer(intm3);
               { let intm1 = allocate('intm1', 2, [

builder/test/visualize/parallel_stencils_2.html
@@ -284,7 +284,7 @@ function pipeline(in1, in2, out) {
   check((((buffer_max(out, 1) - buffer_min(out, 1)) + 5) <= buffer_fold_factor(in2, 1)));
   { let intm4 = allocate('intm4', 2, [
       {bounds:{min:buffer_min(out, 0), max:buffer_max(out, 0)}, stride:2, fold_factor:9223372036854775807},
-      {bounds:{min:buffer_min(out, 1), max:buffer_max(out, 1)}, stride:(((buffer_max(out, 0) - buffer_min(out, 0)) * 2) + 2), fold_factor:2}
+      {bounds:{min:buffer_min(out, 1), max:buffer_max(out, 1)}, stride:(((buffer_max(out, 0) - buffer_min(out, 0)) * 2) + 2), fold_factor:(min((buffer_max(out, 1) - buffer_min(out, 1)), 1) + 1)}
     ]);
     { let intm4_uncropped = clone_buffer(intm4);
       { let intm2 = allocate('intm2', 2, [
@@ -294,7 +294,7 @@ function pipeline(in1, in2, out) {
         { let intm2_uncropped = clone_buffer(intm2);
           { let intm3 = allocate('intm3', 2, [
               {bounds:{min:buffer_min(out, 0), max:buffer_max(out, 0)}, stride:2, fold_factor:9223372036854775807},
-              {bounds:{min:buffer_min(out, 1), max:buffer_max(out, 1)}, stride:(((buffer_max(out, 0) - buffer_min(out, 0)) * 2) + 2), fold_factor:2}
+              {bounds:{min:buffer_min(out, 1), max:buffer_max(out, 1)}, stride:(((buffer_max(out, 0) - buffer_min(out, 0)) * 2) + 2), fold_factor:(min((buffer_max(out, 1) - buffer_min(out, 1)), 1) + 1)}
             ]);
             { let intm3_uncropped = clone_buffer(intm3);
               { let intm1 = allocate('intm1', 2, [

builder/simplify.cc
@@ -320,15 +320,6 @@ public:
           return;
         }
       }
-    } else if (op->intrinsic == intrinsic::abs) {
-      assert(args.size() == 1);
-      if (is_non_negative(args_bounds[0].min)) {
-        set_result(args[0], std::move(args_bounds[0]));
-        return;
-      } else if (is_non_positive(args_bounds[0].max)) {
-        mutate_and_set_result(-args[0]);
-        return;
-      }
     }
 
     expr e = simplify(op, op->intrinsic, std::move(args));

builder/simplify_bounds.cc
@@ -10,14 +10,22 @@ namespace {
 
 template <typename T>
 interval_expr bounds_of_linear(const T* op, interval_expr a, interval_expr b) {
-  return {simplify(op, std::move(a.min), std::move(b.min)), simplify(op, std::move(a.max), std::move(b.max))};
+  if (a.is_point() && b.is_point()) {
+    return point(simplify(op, std::move(a.min), std::move(b.min)));
+  } else {
+    return {simplify(op, std::move(a.min), std::move(b.min)), simplify(op, std::move(a.max), std::move(b.max))};
+  }
 }
 
 template <typename T>
 interval_expr bounds_of_less(const T* op, interval_expr a, interval_expr b) {
-  // This bit of genius comes from
-  // https://github.com/halide/Halide/blob/61b8d384b2b799cd47634e4a3b67aa7c7f580a46/src/Bounds.cpp#L829
-  return {simplify(op, std::move(a.max), std::move(b.min)), simplify(op, std::move(a.min), std::move(b.max))};
+  if (a.is_point() && b.is_point()) {
+    return point(simplify(op, std::move(a.min), std::move(b.min)));
+  } else {
+    // This bit of genius comes from
+    // https://github.com/halide/Halide/blob/61b8d384b2b799cd47634e4a3b67aa7c7f580a46/src/Bounds.cpp#L829
+    return {simplify(op, std::move(a.max), std::move(b.min)), simplify(op, std::move(a.min), std::move(b.max))};
+  }
 }
 
 }  // namespace
@@ -26,11 +34,17 @@ interval_expr bounds_of(const add* op, interval_expr a, interval_expr b) {
   return bounds_of_linear(op, std::move(a), std::move(b));
 }
 interval_expr bounds_of(const sub* op, interval_expr a, interval_expr b) {
-  return {simplify(op, std::move(a.min), std::move(b.max)), simplify(op, std::move(a.max), std::move(b.min))};
+  if (a.is_point() && b.is_point()) {
+    return point(simplify(op, std::move(a.min), std::move(b.min)));
+  } else {
+    return {simplify(op, std::move(a.min), std::move(b.max)), simplify(op, std::move(a.max), std::move(b.min))};
+  }
 }
 interval_expr bounds_of(const mul* op, interval_expr a, interval_expr b) {
   // TODO: I'm pretty sure there are cases missing here that would produce simpler bounds than the fallback cases.
-  if (is_non_negative(a.min) && is_non_negative(b.min)) {
+  if (a.is_point() && b.is_point()) {
+    return point(simplify(op, std::move(a.min), std::move(b.min)));
+  } else if (is_non_negative(a.min) && is_non_negative(b.min)) {
     // Both are >= 0, neither intervals flip.
     return {simplify(op, a.min, b.min), simplify(op, a.max, b.max)};
   } else if (is_non_positive(a.max) && is_non_positive(b.max)) {
@@ -105,6 +119,8 @@ interval_expr bounds_of(const div* op, interval_expr a, interval_expr b) {
   if (b.is_point()) {
     if (is_zero(b.min)) {
       return {0, 0};
+    } else if (a.is_point()) {
+      return point(simplify(op, a.min, b.min));
     } else if (is_non_negative(b.min)) {
       return {simplify(op, a.min, b.min), simplify(op, a.max, b.min)};
     } else if (is_non_positive(b.min)) {
@@ -134,6 +150,9 @@ interval_expr bounds_of(const div* op, interval_expr a, interval_expr b) {
   return union_x_negate_x(std::move(a));
 }
 interval_expr bounds_of(const mod* op, interval_expr a, interval_expr b) {
+  if (a.is_point() && b.is_point()) {
+    return point(simplify(op, std::move(a.min), std::move(b.min)));
+  }
   expr max_b;
   if (is_non_negative(b.min)) {
     max_b = b.max;
@@ -171,14 +190,22 @@ interval_expr bounds_of(const less_equal* op, interval_expr a, interval_expr b) 
   return bounds_of_less(op, std::move(a), std::move(b));
 }
 interval_expr bounds_of(const equal* op, interval_expr a, interval_expr b) {
-  return {0, simplify(static_cast<const logical_and*>(nullptr),
-                 simplify(static_cast<const less_equal*>(nullptr), a.min, b.max),
-                 simplify(static_cast<const less_equal*>(nullptr), b.min, a.max))};
+  if (a.is_point() && b.is_point()) {
+    return point(simplify(op, std::move(a.min), std::move(b.min)));
+  } else {
+    return {0, simplify(static_cast<const logical_and*>(nullptr),
+                   simplify(static_cast<const less_equal*>(nullptr), a.min, b.max),
+                   simplify(static_cast<const less_equal*>(nullptr), b.min, a.max))};
+  }
 }
 interval_expr bounds_of(const not_equal* op, interval_expr a, interval_expr b) {
-  return {simplify(static_cast<const logical_or*>(nullptr), simplify(static_cast<const less*>(nullptr), a.max, b.min),
-              simplify(static_cast<const less*>(nullptr), b.max, a.min)),
-      1};
+  if (a.is_point() && b.is_point()) {
+    return point(simplify(op, std::move(a.min), std::move(b.min)));
+  } else {
+    return {simplify(static_cast<const logical_or*>(nullptr), simplify(static_cast<const less*>(nullptr), a.max, b.min),
+                simplify(static_cast<const less*>(nullptr), b.max, a.min)),
+        1};
+  }
 }
 
 interval_expr bounds_of(const logical_and* op, interval_expr a, interval_expr b) {
@@ -188,11 +215,17 @@ interval_expr bounds_of(const logical_or* op, interval_expr a, interval_expr b) 
   return bounds_of_linear(op, std::move(a), std::move(b));
 }
 interval_expr bounds_of(const logical_not* op, interval_expr a) {
-  return {simplify(op, std::move(a.max)), simplify(op, std::move(a.min))};
+  if (a.is_point()) {
+    return point(simplify(op, std::move(a.min)));
+  } else {
+    return {simplify(op, std::move(a.max)), simplify(op, std::move(a.min))};
+  }
 }
 
 interval_expr bounds_of(const class select* op, interval_expr c, interval_expr t, interval_expr f) {
-  if (is_true(c.min)) {
+  if (c.is_point() && t.is_point() && f.is_point()) {
+    return point(simplify(op, std::move(c.min), std::move(t.min), std::move(f.min)));
+  } else if (is_true(c.min)) {
     return t;
   } else if (is_false(c.max)) {
     return f;
@@ -207,6 +240,8 @@ interval_expr bounds_of(const call* op, std::vector<interval_expr> args) {
     assert(args.size() == 1);
     if (is_non_negative(args[0].min)) {
       return {args[0].min, args[0].max};
+    } else if (args[0].is_point()) {
+      return point(simplify(op, intrinsic::abs, {std::move(args[0].min)}));
     } else if (is_non_positive(args[0].max)) {
       return {negate(args[0].max), negate(args[0].min)};
     } else {

builder/simplify_exprs.cc
@@ -700,6 +700,15 @@ expr simplify(const call* op, intrinsic fn, std::vector<expr> args) {
     }
   }
 
+  if (fn == intrinsic::abs) {
+    assert(args.size() == 1);
+    if (is_non_negative(args[0])) {
+      return args[0];
+    } else if (is_non_positive(args[0])) {
+      return simplify(static_cast<const sub*>(nullptr), 0, std::move(args[0]));
+    }
+  }
+
   expr e;
   if (!changed) {
     assert(op);

runtime/expr.h
@@ -526,9 +526,9 @@ inline bool is_true(const expr& x) {
 SLINKY_ALWAYS_INLINE inline bool is_false(const expr& x) { return is_zero(x); }
 
 // Check if `x` is a call to the intrinsic `fn`.
-inline bool is_intrinsic(const expr& x, intrinsic fn) {
+inline const call* is_intrinsic(const expr& x, intrinsic fn) {
   const call* c = x.as<call>();
-  return c ? c->intrinsic == fn : false;
+  return c && c->intrinsic == fn ? c : nullptr;
 }
 bool is_buffer_min(const expr& x, var sym, int dim);
 bool is_buffer_max(const expr& x, var sym, int dim);
@@ -552,12 +552,14 @@ const expr& indeterminate();
 
 inline bool is_positive(const expr& x) {
   if (is_positive_infinity(x)) return true;
+  if (const call* c = is_intrinsic(x, intrinsic::abs)) return is_positive(c);
   const index_t* c = as_constant(x);
   return c ? *c > 0 : false;
 }
 
 inline bool is_non_negative(const expr& x) {
   if (is_positive_infinity(x)) return true;
+  if (is_intrinsic(x, intrinsic::abs)) return true;
   const index_t* c = as_constant(x);
   return c ? *c >= 0 : false;
 }

commit b1c337d4a6554f6b61cc962c6e018b05e8fccfed

Ignore loop bounds for sliding bounds and only use finite fold factors (#232)
This fixes a regression introduced in #230, and #231 didn't quite fix it
either.

builder/slide_and_fold_storage.cc
@@ -234,6 +234,9 @@ public:
         std::optional<box_expr>& bounds = (*loop.buffer_bounds)[output];
         if (!bounds) continue;
 
+        // We don't want to use the bounds of the loop we are sliding over here.
+        auto ignore_loop_bounds = set_value_in_scope(*loop.expr_bounds, loop.sym, interval_expr::all());
+
         expr loop_var = variable::make(loop.sym);
 
         for (int d = 0; d < static_cast<int>(bounds->size()); ++d) {
@@ -260,7 +263,7 @@ public:
             // The bounds of each loop iteration do not overlap. We can't re-use work between loop iterations, but we
             // can fold the storage.
             expr fold_factor = simplify(bounds_of(ignore_loop_max(cur_bounds_d.extent()), *loop.expr_bounds).max);
-            if (!depends_on(fold_factor, loop.sym).any()) {
+            if (is_finite(fold_factor) && !depends_on(fold_factor, loop.sym).any()) {
               if (loop.add_synchronization()) {
                 // We need a fold per worker when parallelizing the loop.
                 // TODO: This extra folding seems excessive, it allows all workers to execute any stage.
@@ -296,7 +299,7 @@ public:
 
             if (!did_overlapped_fold) {
               expr fold_factor = simplify(bounds_of(ignore_loop_max(cur_bounds_d.extent()), *loop.expr_bounds).max);
-              if (!depends_on(fold_factor, loop.sym).any()) {
+              if (is_finite(fold_factor) && !depends_on(fold_factor, loop.sym).any()) {
                 if (synchronize) {
                   // We need an extra fold per worker when parallelizing the loop.
                   // TODO: This extra folding seems excessive, it allows all workers to execute any stage.

builder/test/visualize/parallel_stencils_1.html
@@ -284,7 +284,7 @@ function pipeline(in1, in2, out) {
   check((((buffer_max(out, 1) - buffer_min(out, 1)) + 5) <= buffer_fold_factor(in2, 1)));
   { let intm4 = allocate('intm4', 2, [
       {bounds:{min:buffer_min(out, 0), max:buffer_max(out, 0)}, stride:2, fold_factor:9223372036854775807},
-      {bounds:{min:buffer_min(out, 1), max:buffer_max(out, 1)}, stride:(((buffer_max(out, 0) - buffer_min(out, 0)) * 2) + 2), fold_factor:(min((buffer_max(out, 1) - buffer_min(out, 1)), 1) + 1)}
+      {bounds:{min:buffer_min(out, 1), max:buffer_max(out, 1)}, stride:(((buffer_max(out, 0) - buffer_min(out, 0)) * 2) + 2), fold_factor:2}
     ]);
     { let intm4_uncropped = clone_buffer(intm4);
       { let intm2 = allocate('intm2', 2, [
@@ -294,7 +294,7 @@ function pipeline(in1, in2, out) {
         { let intm2_uncropped = clone_buffer(intm2);
           { let intm3 = allocate('intm3', 2, [
               {bounds:{min:buffer_min(out, 0), max:buffer_max(out, 0)}, stride:2, fold_factor:9223372036854775807},
-              {bounds:{min:buffer_min(out, 1), max:buffer_max(out, 1)}, stride:(((buffer_max(out, 0) - buffer_min(out, 0)) * 2) + 2), fold_factor:((buffer_max(out, 1) - buffer_min(out, 1)) + 1)}
+              {bounds:{min:buffer_min(out, 1), max:buffer_max(out, 1)}, stride:(((buffer_max(out, 0) - buffer_min(out, 0)) * 2) + 2), fold_factor:2}
             ]);
             { let intm3_uncropped = clone_buffer(intm3);
               { let intm1 = allocate('intm1', 2, [

builder/test/visualize/parallel_stencils_2.html
@@ -284,7 +284,7 @@ function pipeline(in1, in2, out) {
   check((((buffer_max(out, 1) - buffer_min(out, 1)) + 5) <= buffer_fold_factor(in2, 1)));
   { let intm4 = allocate('intm4', 2, [
       {bounds:{min:buffer_min(out, 0), max:buffer_max(out, 0)}, stride:2, fold_factor:9223372036854775807},
-      {bounds:{min:buffer_min(out, 1), max:buffer_max(out, 1)}, stride:(((buffer_max(out, 0) - buffer_min(out, 0)) * 2) + 2), fold_factor:(min((buffer_max(out, 1) - buffer_min(out, 1)), 1) + 1)}
+      {bounds:{min:buffer_min(out, 1), max:buffer_max(out, 1)}, stride:(((buffer_max(out, 0) - buffer_min(out, 0)) * 2) + 2), fold_factor:2}
     ]);
     { let intm4_uncropped = clone_buffer(intm4);
       { let intm2 = allocate('intm2', 2, [
@@ -294,7 +294,7 @@ function pipeline(in1, in2, out) {
         { let intm2_uncropped = clone_buffer(intm2);
           { let intm3 = allocate('intm3', 2, [
               {bounds:{min:buffer_min(out, 0), max:buffer_max(out, 0)}, stride:2, fold_factor:9223372036854775807},
-              {bounds:{min:buffer_min(out, 1), max:buffer_max(out, 1)}, stride:(((buffer_max(out, 0) - buffer_min(out, 0)) * 2) + 2), fold_factor:(min((buffer_max(out, 1) - buffer_min(out, 1)), 1) + 1)}
+              {bounds:{min:buffer_min(out, 1), max:buffer_max(out, 1)}, stride:(((buffer_max(out, 0) - buffer_min(out, 0)) * 2) + 2), fold_factor:2}
             ]);
             { let intm3_uncropped = clone_buffer(intm3);
               { let intm1 = allocate('intm1', 2, [