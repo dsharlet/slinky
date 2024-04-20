8e0f0a92cabaeb7aecb574b8fd6f0c778a5bb229
Dillon <dsharlet@gmail.com>
2024-04-19 20:09:00 -0700

    Another == rule

--- builder/simplify_exprs.cc
+++ builder/simplify_exprs.cc
@@ -665,6 +665,7 @@ expr simplify(const equal* op, expr a, expr b) {
       r.rewrite(x * c0 == c1, x == eval(c1 / c0), eval(c1 % c0 == 0)) ||
       r.rewrite(x + c0 == y + c1, x == y + eval(c1 - c0)) ||
       r.rewrite(x + c0 == c1, x == eval(c1 - c0)) ||
+      r.rewrite(x + c0 == c1 - y, x + y == eval(c1 - c0)) ||
       r.rewrite(c0 - x == c1, x == eval(c0 - c1)) ||
       false) {
     return r.result;
