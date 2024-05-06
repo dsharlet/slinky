7f5a99a3eaf0accc88be97a746998944b919672b
Dillon <dsharlet@gmail.com>
2024-05-06 10:07:38 -0700

    Use ASSERT_THAT for simplify/substitute tests (#284)
    Replace `test_simplify`, `test_substitute`, and similar helpers with
    uses of ASSERT_THAT and a custom matcher.
    
    The motivation for this: I'm working on a large change to the
    simplifier, these tests are failing often and need a lot of debugging.
    It's really annoying to figure out exactly which test is failing,
    because the reported failure line is in `test_substitute` or similar. We
    can't simply use ASSERT_EQ because we overload operator== for expr in a
    different way. Calling `match` via an ASSERT_THAT matcher is easy and
    solves all these problems.
    
    There was similar discussion on #279 but I think this use is much
    lighter weight.

--- builder/test/substitute.cc
+++ builder/test/substitute.cc
@@ -1,3 +1,4 @@
+#include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
 #include <cassert>
@@ -21,66 +22,44 @@ var u(symbols, "u");
 
 }  // namespace
 
-template <typename T>
-void test_substitute(const expr& test, T target, const expr& replacement, const expr& expected) {
-  expr result = substitute(test, target, replacement);
-  if (!match(result, expected)) {
-    std::cout << "substitute failed" << std::endl;
-    std::cout << test << std::endl;
-    std::cout << "got: " << std::endl;
-    std::cout << result << std::endl;
-    std::cout << "expected: " << std::endl;
-    std::cout << expected << std::endl;
-    ASSERT_TRUE(false);
-  }
-}
-
-template <typename T>
-void test_substitute(const stmt& test, T target, const expr& replacement, const stmt& expected) {
-  stmt result = substitute(test, target, replacement);
-  if (!match(result, expected)) {
-    std::cout << "substitute failed" << std::endl;
-    std::cout << test << std::endl;
-    std::cout << "got: " << std::endl;
-    std::cout << result << std::endl;
-    std::cout << "expected: " << std::endl;
-    std::cout << expected << std::endl;
-    ASSERT_TRUE(false);
-  }
-}
+MATCHER_P(matches, x, "") { return match(arg, x); }
 
 TEST(substitute, basic) {
-  test_substitute(x + y, x, z, z + y);
-  test_substitute(check::make(y == buffer_min(x, 3)), buffer_min(x, 3), z, check::make(expr(y) == expr(z)));
+  ASSERT_THAT(substitute(x + y, x, z), matches(z + y));
+  ASSERT_THAT(
+      substitute(check::make(y == buffer_min(x, 3)), buffer_min(x, 3), z), matches(check::make(expr(y) == expr(z))));
 }
 
 TEST(substitute, shadowed) {
-  test_substitute(let::make(x, y, x + z), x, w, let::make(x, y, x + z));
-  test_substitute(let::make({{x, 1}, {y, 2}}, z + 1), z, z + w, let::make({{x, 1}, {y, 2}}, z + w + 1));
+  ASSERT_THAT(substitute(let::make(x, y, x + z), x, w), matches(let::make(x, y, x + z)));
+  ASSERT_THAT(
+      substitute(let::make({{x, 1}, {y, 2}}, z + 1), z, z + w), matches(let::make({{x, 1}, {y, 2}}, z + w + 1)));
 
-  test_substitute(crop_dim::make(x, 1, {y, z}, check::make(0 < buffer_min(x, 1))), buffer_min(x, 1), w,
-      crop_dim::make(x, 1, {max(y, w), z}, check::make(0 < buffer_min(x, 1))));
+  ASSERT_THAT(substitute(crop_dim::make(x, 1, {y, z}, check::make(0 < buffer_min(x, 1))), buffer_min(x, 1), w),
+      matches(crop_dim::make(x, 1, {max(y, w), z}, check::make(0 < buffer_min(x, 1)))));
 
-  test_substitute(slice_dim::make(x, 2, 0, check::make(buffer_min(x, 3) == 0)), buffer_min(x, 3), 1,
-      slice_dim::make(x, 2, 0, check::make(buffer_min(x, 3) == 0)));
-  test_substitute(slice_dim::make(x, 2, 0, check::make(y == buffer_min(x, 3))), y, buffer_max(x, 3),
-      slice_dim::make(x, 2, 0, check::make(buffer_max(x, 2) == buffer_min(x, 3))));
-  test_substitute(slice_dim::make(x, 2, 0, check::make(y == buffer_min(x, 3))), y, buffer_max(x, 2),
-      slice_dim::make(x, 2, 0, check::make(expr() == buffer_min(x, 3))));
-  test_substitute(slice_dim::make(x, 2, 0, check::make(y == buffer_min(x, 3))), y, buffer_max(x, 1),
-      slice_dim::make(x, 2, 0, check::make(buffer_max(x, 1) == buffer_min(x, 3))));
+  ASSERT_THAT(substitute(slice_dim::make(x, 2, 0, check::make(buffer_min(x, 3) == 0)), buffer_min(x, 3), 1),
+      matches(slice_dim::make(x, 2, 0, check::make(buffer_min(x, 3) == 0))));
+  ASSERT_THAT(substitute(slice_dim::make(x, 2, 0, check::make(y == buffer_min(x, 3))), y, buffer_max(x, 3)),
+      matches(slice_dim::make(x, 2, 0, check::make(buffer_max(x, 2) == buffer_min(x, 3)))));
+  ASSERT_THAT(substitute(slice_dim::make(x, 2, 0, check::make(y == buffer_min(x, 3))), y, buffer_max(x, 2)),
+      matches(slice_dim::make(x, 2, 0, check::make(expr() == buffer_min(x, 3)))));
+  ASSERT_THAT(substitute(slice_dim::make(x, 2, 0, check::make(y == buffer_min(x, 3))), y, buffer_max(x, 1)),
+      matches(slice_dim::make(x, 2, 0, check::make(buffer_max(x, 1) == buffer_min(x, 3)))));
 
-  test_substitute(copy_stmt::make(x, {y, z}, w, {y, z}, {}), y, z, copy_stmt::make(x, {y, z}, w, {y, z}, {}));
-  test_substitute(copy_stmt::make(x, {y}, w, {y}, {}), y, z, copy_stmt::make(x, {y}, w, {y}, {}));
-  test_substitute(copy_stmt::make(x, {y}, w, {z}, {}), y, u, copy_stmt::make(x, {u}, w, {z}, {}));
+  ASSERT_THAT(
+      substitute(copy_stmt::make(x, {y, z}, w, {y, z}, {}), y, z), matches(copy_stmt::make(x, {y, z}, w, {y, z}, {})));
+  ASSERT_THAT(substitute(copy_stmt::make(x, {y}, w, {y}, {}), y, z), matches(copy_stmt::make(x, {y}, w, {y}, {})));
+  ASSERT_THAT(substitute(copy_stmt::make(x, {y}, w, {z}, {}), y, u), matches(copy_stmt::make(x, {u}, w, {z}, {})));
 }
 
 TEST(substitute, implicit_bounds) {
-  test_substitute(crop_dim::make(x, 0, bounds(y, z), check::make(x)), buffer_min(x, 0), w,
-      crop_dim::make(x, 0, bounds(max(y, w), z), check::make(x)));
-  test_substitute(crop_dim::make(x, 0, bounds(y, z), check::make(x)), buffer_max(x, 0), w,
-      crop_dim::make(x, 0, bounds(y, min(z, w)), check::make(x)));
-  test_substitute(buffer_at(x), buffer_min(x, 2), y, buffer_at(x, std::vector<expr>{expr(), expr(), expr(y)}));
+  ASSERT_THAT(substitute(crop_dim::make(x, 0, bounds(y, z), check::make(x)), buffer_min(x, 0), w),
+      matches(crop_dim::make(x, 0, bounds(max(y, w), z), check::make(x))));
+  ASSERT_THAT(substitute(crop_dim::make(x, 0, bounds(y, z), check::make(x)), buffer_max(x, 0), w),
+      matches(crop_dim::make(x, 0, bounds(y, min(z, w)), check::make(x))));
+  ASSERT_THAT(
+      substitute(buffer_at(x), buffer_min(x, 2), y), matches(buffer_at(x, std::vector<expr>{expr(), expr(), expr(y)})));
 }
 
 TEST(match, basic) {
