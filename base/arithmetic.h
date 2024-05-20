401475a68cbc3f6f7a81b2fb35b4e25682e1143d
Dillon <dsharlet@gmail.com>
2024-05-20 14:42:15 -0700

    The _p version of this isn't available on clang

--- base/arithmetic.h
+++ base/arithmetic.h
@@ -127,17 +127,20 @@ inline T saturate_mod(T a, T b) {
 
 template <class T>
 bool add_overflows(T a, T b) {
-  return __builtin_add_overflow_p(a, b, 0);
+  T dummy;
+  return __builtin_add_overflow(a, b, &dummy);
 }
 
 template <class T>
 bool sub_overflows(T a, T b) {
-  return __builtin_sub_overflow_p(a, b, 0);
+  T dummy;
+  return __builtin_sub_overflow(a, b, &dummy);
 }
 
 template <class T>
 bool mul_overflows(T a, T b) {
-  return __builtin_mul_overflow_p(a, b, 0);
+  T dummy;
+  return __builtin_mul_overflow(a, b, &dummy);
 }
 
 template <typename T>
