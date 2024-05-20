40ebd04f16560f778c3c947a428f7bf439b15504
Dillon <dsharlet@gmail.com>
2024-05-20 00:14:48 -0700

    These rules seem good to keep

--- builder/simplify_exprs.cc
+++ builder/simplify_exprs.cc
@@ -141,6 +141,8 @@ expr simplify(const class min* op, expr a, expr b) {
       r.rewrite(min(x, select(y, z, min(x, w))), min(x, select(y, z, w))) ||
       r.rewrite(min(x, select(y, max(x, z), w)), select(y, x, min(x, w))) ||
       r.rewrite(min(x, select(y, z, max(x, w))), select(y, min(x, z), w)) ||
+      r.rewrite(min(y, select(x, y, w)), select(x, y, min(y, w))) ||
+      r.rewrite(min(z, select(x, w, z)), select(x, min(z, w), z)) ||
       r.rewrite(min(select(x, y, z), select(x, w, u)), select(x, min(y, w), min(z, u))) ||
       false) {
     return r.result;
@@ -266,6 +268,8 @@ expr simplify(const class max* op, expr a, expr b) {
       r.rewrite(max(x, select(y, z, max(x, w))), max(x, select(y, z, w))) ||
       r.rewrite(max(x, select(y, min(x, z), w)), select(y, x, max(x, w))) ||
       r.rewrite(max(x, select(y, z, min(x, w))), select(y, max(x, z), w)) ||
+      r.rewrite(max(y, select(x, y, w)), select(x, y, max(y, w))) ||
+      r.rewrite(max(z, select(x, w, z)), select(x, max(z, w), z)) ||
       r.rewrite(max(select(x, y, z), select(x, w, u)), select(x, max(y, w), max(z, u))) ||
       false) {
     return r.result;
