63d5f6100855f3c9b8cd688f354a8faa46eeca8a
Aelphy <aelphy@google.com>
2024-05-24 10:46:34 +0200

    fixed buffer slicing

--- runtime/buffer.h
+++ runtime/buffer.h
@@ -242,10 +242,12 @@ public:
     for (std::size_t i = slice_leading; i < ds.size(); ++i) {
       std::size_t d = ds[i];
       std::size_t next_d = i + 1 < ds.size() ? ds[i + 1] : rank;
+      assert(d < rank);
+      assert(next_d <= rank);
 
       // Move the dimensions between this slice and the next slice down by the number of slices we've done so far.
       for (std::size_t j = d; j + 1 < next_d; ++j) {
-        dims[j] = dims[j + i + 1];
+        dims[j] = dims[j + 1];
       }
       --rank;
     }
