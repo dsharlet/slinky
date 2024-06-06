035a566e8c6667fb4f41cfdf9d74dc2c77c45dec
Volodymyr Kysenko <vksnk@google.com>
2024-06-06 21:08:08 +0000

    Don't clamp loop bounds

--- builder/pipeline.cc
+++ builder/pipeline.cc
@@ -622,7 +622,7 @@ class pipeline_builder {
         if (o.dims[d] == loop.sym()) {
           expr loop_max = loop_bounds.max;
           expr loop_step = sanitizer_.mutate(loop.step);
-          interval_expr bounds = slinky::bounds(loop.var, min(simplify(loop.var + loop_step - 1), loop_max));
+          interval_expr bounds = slinky::bounds(loop.var, simplify(loop.var + loop_step - 1));
           body = crop_dim::make(o.sym(), o.sym(), d, bounds, body);
         }
       }
