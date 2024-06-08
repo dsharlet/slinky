e2b1c0194822527f7d481d249c310ec9fa8386db
Dillon <dsharlet@gmail.com>
2024-06-07 17:07:20 -0700

    Add rules for simplifying nested loops with broadcasts (#400)

--- builder/simplify_rules.h
+++ builder/simplify_rules.h
@@ -614,6 +614,12 @@ bool apply_less_rules(Fn&& apply) {
       apply(y < select(x, y, w), select(x, false, y < w)) ||
       apply(w < select(x, y, w), select(x, w < y, false)) ||
       apply(select(x, y, z) < select(x, w, u), select(x, y < w, z < u)) ||
+      apply(select(x, y, z) < v + select(x, w, u), select(x, y < v + w, z < v + u)) ||
+      apply(select(x, y, z) < v - select(x, w, u), select(x, y < v - w, z < v - u)) ||
+      apply(select(x, y, z) < select(x, w, u) - v, select(x, y < w - v, z < u - v)) ||
+      apply(v + select(x, y, z) < select(x, w, u), select(x, v + y < w, v + z < u)) ||
+      apply(v - select(x, y, z) < select(x, w, u), select(x, v - y < w, v - z < u)) ||
+      apply(select(x, y, z) - v < select(x, w, u), select(x, y - v < w, z - v < u)) ||
 
       // Nested logicals
       apply(x < y, y && !x, is_boolean(x) && is_boolean(y)) ||
@@ -662,6 +668,9 @@ bool apply_equal_rules(Fn&& apply) {
       apply(x%c0 == c1, false, eval(c0 > 0 && (c1 >= c0 || c1 < 0))) ||
     
       apply(select(x, y, z) == select(x, w, u), select(x, y == w, z == u)) ||
+      apply(v + select(x, y, z) == select(x, w, u), select(x, w == v + y, u == v + z)) ||
+      apply(v - select(x, y, z) == select(x, w, u), select(x, w == v - y, u == v - z)) ||
+      apply(select(x, y, z) - v == select(x, w, u), select(x, w == y - v, u == z - v)) ||
       apply(select(x, c0, y) == c1, select(x, eval(c0 == c1), y == c1)) ||
       apply(select(x, y, c0) == c1, select(x, y == c1, eval(c0 == c1))) ||
       apply(y == select(x == y, x, z), x == y || y == z) ||
