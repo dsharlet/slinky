e61514090f7f059e4cf81612b37dc7017c2021fa
Dillon <dsharlet@gmail.com>
2024-06-12 22:29:03 -0700

    Hopefully reduce overhead of chrome_trace

--- base/chrome_trace.cc
+++ base/chrome_trace.cc
@@ -3,6 +3,7 @@
 #include <chrono>
 #include <cstdint>
 #include <fstream>
+#include <sstream>
 #include <thread>
 
 namespace slinky {
@@ -14,16 +15,38 @@ chrome_trace::chrome_trace(std::ostream& os) : os_(os), event_written_(false) {
 chrome_trace::~chrome_trace() { os_ << "]\n"; }
 
 void chrome_trace::write_event(const char* name, const char* cat, char type) {
-  auto tid = std::this_thread::get_id();
   auto t = std::chrono::high_resolution_clock::now();
   auto ts = std::chrono::duration_cast<std::chrono::microseconds>(t - t0_).count();
+
+  // The only way to convert a thread ID to a string is via operator<<, which is slow, so do it once as a thread local.
+  thread_local std::string tid_str = []() {
+    auto tid = std::this_thread::get_id();
+    std::stringstream ss;
+    ss << tid;
+    return ss.str();
+  }();
+
+  // The idea here is to assemble a buffer of what we want to write, then lock the mutex, and write our buffer.
+  thread_local std::string buffer;
+  buffer.clear();
+  buffer += "{\"name\":\"";
+  buffer += name;
+  buffer += "\",\"cat\":\"";
+  buffer += cat;
+  buffer += "\",\"ph\":\"";
+  buffer += type;
+  buffer += "\",\"pid\":0,\"tid\":";
+  buffer += tid_str;
+  buffer += ",\"ts\":";
+  buffer += std::to_string(ts);
+  buffer += '}';
+
   std::unique_lock l(mtx_);
   if (event_written_) {
     os_ << ",\n";
   }
   event_written_ = true;
-  os_ << "{\"name\":\"" << name << "\",\"cat\":\"" << cat << "\",\"ph\":\"" << type << "\",\"pid\":0,\"tid\":" << tid
-      << ",\"ts\":" << ts << "}";
+  os_ << buffer;
 }
 
 void chrome_trace::begin(const char* name) { write_event(name, "stmt", 'B'); }
