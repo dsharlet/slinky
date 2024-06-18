57abe27916fe2055404bacc45f2fd0bd1934fe18
Dillon <dsharlet@gmail.com>
2024-06-18 10:50:19 -0700

    Partially revert #409

--- builder/pipeline.cc
+++ builder/pipeline.cc
@@ -253,6 +253,11 @@ public:
       return;
     }
 
+    // Don't lift internally allocated buffer metadata expressions.
+    assert(op->args.size() >= 1 && as_variable(op->args[0]));
+    // TODO: This should be a proper API error.
+    assert(external.count(*as_variable(op->args[0])));
+
     auto i = replacements.insert(std::pair<const expr, var>(op, 0));
     if (i.second) {
       i.first->second = ctx.insert_unique("g");
@@ -918,8 +923,8 @@ stmt build_pipeline(node_context& ctx, const std::vector<buffer_expr_ptr>& input
   stmt result;
   result = builder.build(result, nullptr, loop_id());
   result = builder.add_input_checks(result);
-  result = builder.define_sanitized_replacements(result);
   result = builder.make_buffers(result);
+  result = builder.define_sanitized_replacements(result);
 
   result = slide_and_fold_storage(result, ctx);
 
