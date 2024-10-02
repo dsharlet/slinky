d75b9301a9f412d3e5a067aef0074631d60b58f6
Volodymyr Kysenko <vksnk@google.com>
2024-10-02 22:54:19 +0000

    Use set

--- builder/optimizations.cc
+++ builder/optimizations.cc
@@ -452,15 +452,9 @@ public:
         output_info->maybe_alias(in, std::move(back));
       }
     } else if (op->attrs.allow_in_place) {
-      for (int ix = 0; ix < op->inputs.size(); ++ix) {
-        var i = op->inputs[ix];
-        bool same_input = false;
-        for (int iy = 0; iy < ix; ++iy) {
-          same_input = same_input || (op->inputs[iy] == i);
-        }
         // If input is repeated, we don't want to add into the alias info again.
-        if (same_input) continue;
-
+        std::set<var> unique_inputs(op->inputs.begin(), op->inputs.end());
+        for (var i : unique_inputs) {         
         std::optional<buffer_info>& input_info = buffers[i];
         if (!input_info || input_info->is_input) {
           // We can't write to this buffer.
