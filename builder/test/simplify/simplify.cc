eb8398d46e15ebdd99a6b4ed796d4cfbaf880a69
Dillon <dsharlet@gmail.com>
2024-10-15 17:56:42 -0700

    Add some minor select rules

--- builder/test/simplify/simplify.cc
+++ builder/test/simplify/simplify.cc
@@ -204,6 +204,8 @@ TEST(simplify, basic) {
   ASSERT_THAT(simplify(select(x, y + 3, 5) - 1), matches(select(x, y + 2, 4)));
   ASSERT_THAT(simplify(min(x + 2, select(y, 3, z + 4)) - 1), matches(min(x + 1, select(y, 2, z + 3))));
 
+  ASSERT_THAT(simplify(select((y <= 0), select((x <= 0), z, x), z)), matches(select(0 < x && y <= 0, x, z)));
+
   ASSERT_THAT(simplify(crop_dim::make(y, x, 1, {expr(), expr()}, call_stmt::make(nullptr, {}, {y}, {}))),
       matches(call_stmt::make(nullptr, {}, {x}, {})));
   ASSERT_THAT(simplify(crop_buffer::make(y, x, {}, call_stmt::make(nullptr, {}, {y}, {}))),
