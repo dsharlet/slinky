5c2063868cb6de4a3b0c71374e484c19f1636842
Dillon <dsharlet@gmail.com>
2024-11-03 12:31:31 -0800

    Remove obsolete optimization/complexity

--- base/chrome_trace.cc
+++ base/chrome_trace.cc
@@ -11,8 +11,6 @@ namespace slinky {
 
 namespace {
 
-std::atomic<int> next_id = 0;
-
 // Unfortunately, std::clock returns the CPU time for the whole process, not the current thread.
 std::clock_t clock_per_thread_us() {
   #ifdef _MSC_VER
@@ -27,17 +25,13 @@ std::clock_t clock_per_thread_us() {
 
 }  // namespace
 
-chrome_trace::chrome_trace(std::ostream& os) : os_(os), id_(next_id++) {
+chrome_trace::chrome_trace(std::ostream& os) : os_(os) {
   os_ << "[{\"name\":\"chrome_trace\",\"cat\":\"slinky\",\"ph\":\"B\",\"pid\":0,\"tid\":0,\"ts\":0}";
   os_ << ",\n{\"name\":\"chrome_trace\",\"cat\":\"slinky\",\"ph\":\"E\",\"pid\":0,\"tid\":0,\"ts\":0}";
   t0_ = std::chrono::high_resolution_clock::now();
   cpu_t0_ = clock_per_thread_us();
 }
 chrome_trace::~chrome_trace() {
-  // Flush any unwritten buffers.
-  for (const auto& i : buffers_) {
-    os_ << i.second;
-  }
   os_ << "]\n";
 }
 
@@ -56,40 +50,27 @@ void chrome_trace::write_event(const char* name, const char* cat, char type) {
     return ss.str();
   }();
 
-  // To avoid overhead when multiple threads are writing traces, we try to keep a pointer to the buffer for this trace
-  // object and thread cached locally.
-  thread_local int current_buffer_owner = -1;
-  thread_local std::string* buffer = nullptr;
+  thread_local std::string buffer;
 
-  if (!buffer || current_buffer_owner != id_) {
-    // We should only need to pay the cost of this lookup if multiple different chrome_trace objects are writing traces
-    // on the same thread at the same time.
-    std::unique_lock l(mtx_);
-    buffer = &buffers_[std::this_thread::get_id()];
-    current_buffer_owner = id_;
-  }
-
+  buffer.clear();
   // It would be an error to put a comma here as the first item in the output, but we put a dummy {} object at the
   // beginning of the array.
-  *buffer += ",\n{\"name\":\"";
-  *buffer += name;
-  *buffer += "\",\"cat\":\"";
-  *buffer += cat;
-  *buffer += "\",\"ph\":\"";
-  *buffer += type;
-  *buffer += pid_tid_str;
-  *buffer += ",\"ts\":";
-  *buffer += std::to_string(ts);
-  *buffer += ",\"tts\":";
-  *buffer += std::to_string(cpu_ts);
-  *buffer += '}';
+  buffer += ",\n{\"name\":\"";
+  buffer += name;
+  buffer += "\",\"cat\":\"";
+  buffer += cat;
+  buffer += "\",\"ph\":\"";
+  buffer += type;
+  buffer += pid_tid_str;
+  buffer += ",\"ts\":";
+  buffer += std::to_string(ts);
+  buffer += ",\"tts\":";
+  buffer += std::to_string(cpu_ts);
+  buffer += '}';
 
-  if (buffer->size() > 4096 * 16) {
-    // Flush our buffer.
-    std::unique_lock l(mtx_);
-    os_.write(buffer->data(), buffer->size());
-    buffer->clear();
-  }
+  // Flush our buffer.
+  std::unique_lock l(mtx_);
+  os_.write(buffer.data(), buffer.size());
 }
 
 void chrome_trace::begin(const char* name) { write_event(name, "slinky", 'B'); }
