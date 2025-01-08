e12c33bd7191d3f0d99e4a95a7280d6d30c6268c
Dillon <dsharlet@gmail.com>
2025-01-08 12:25:41 -0800

    alias_copy_src should set the permutation too (#542)

--- builder/optimizations.cc
+++ builder/optimizations.cc
@@ -632,6 +632,7 @@ public:
     a.target = op->dst;
     a.at.resize(op->dst_x.size());
     a.dims.resize(op->src_x.size());
+    a.permutation.resize(op->dst_x.size());
     assert(op->src_x.size() == info->dims.size());
     for (int dst_d = 0; dst_d < static_cast<int>(op->dst_x.size()); ++dst_d) {
       int src_d;
@@ -657,6 +658,7 @@ public:
           buffer_fold_factor(op->dst, dst_d),
       };
       a.at[dst_d] = info->dims[src_d].bounds.min - offset;
+      a.permutation[dst_d] = src_d;
     }
 
     for (const dim_expr& d : a.dims) {
@@ -665,10 +667,6 @@ public:
         return;
       }
     }
-
-    // In this case, we just want an identity permutation, because the alias dims are already in the src order.
-    a.permutation.resize(op->dst_x.size());
-    std::iota(a.permutation.begin(), a.permutation.end(), 0);
 
     a.is_copy = true;
     a.may_mutate = false;
