b038782d2f0a9107c518931bba91af1b396d0073
Dillon <dsharlet@gmail.com>
2025-02-10 19:19:33 -0800

    Don't crash if an input is unused

--- builder/test/checks.cc
+++ builder/test/checks.cc
@@ -58,4 +58,35 @@ TEST(pipeline, checks) {
   ASSERT_EQ(checks_failed, 1);
 }
 
+TEST(pipeline, unused_input) {
+  // Make the pipeline
+  node_context ctx;
+
+  auto in = buffer_expr::make(ctx, "in", 1, sizeof(int));
+  auto unused = buffer_expr::make(ctx, "unused", 2, sizeof(int));
+  auto out = buffer_expr::make(ctx, "out", 1, sizeof(int));
+
+  var x(ctx, "x");
+
+  func mul = func::make(multiply_2<int>, {{in, {point(x)}}}, {{out, {x}}});
+
+  pipeline p = build_pipeline(ctx, {in, unused}, {out});
+
+  // Run the pipeline
+  const int N = 10;
+
+  eval_context eval_ctx;
+
+  buffer<int, 1> in_buf({N});
+  buffer<int, 2> unused_buf;
+  buffer<int, 1> out_buf({N});
+  in_buf.allocate();
+  out_buf.allocate();
+
+  const raw_buffer* inputs[] = {&in_buf, &unused_buf};
+  const raw_buffer* outputs[] = {&out_buf};
+  index_t result = p.evaluate(inputs, outputs, eval_ctx);
+  ASSERT_EQ(result, 0);
+}
+
 }  // namespace slinky
