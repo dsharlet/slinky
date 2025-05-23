b7fa10a44353324cc41410bfef93e691ca0a2c57
Dillon <dsharlet@gmail.com>
2025-05-22 22:51:20 -0700

    Properly abstract thread pool loop objects

--- base/thread_pool.cc
+++ base/thread_pool.cc
@@ -9,7 +9,7 @@
 
 namespace slinky {
 
-thread_pool_impl::thread_pool_impl(int workers, task_ref init) : stop_(false) {
+thread_pool_impl::thread_pool_impl(int workers, function_ref<void()> init) : stop_(false) {
   auto worker = [this, init]() {
     if (init) init();
     run_worker([this]() -> bool { return stop_; });
@@ -39,9 +39,9 @@ thread_local std::vector<const void*> task_stack;
 
 }  // namespace
 
-std::shared_ptr<thread_pool::loop> thread_pool_impl::dequeue(loop_task& t) {
+std::shared_ptr<task> thread_pool_impl::dequeue(task_body& t) {
   for (auto i = task_queue_.begin(); i != task_queue_.end();) {
-    std::shared_ptr<thread_pool::loop>& loop = std::get<1>(*i);
+    std::shared_ptr<task_impl>& loop = std::get<1>(*i);
     if (std::find(task_stack.begin(), task_stack.end(), &*loop) != task_stack.end()) {
       // Don't run the same loop multiple times on the same thread.
       ++i;
@@ -60,7 +60,7 @@ std::shared_ptr<thread_pool::loop> thread_pool_impl::dequeue(loop_task& t) {
     if (--max_workers == 0 || iterations_remaining == 1) {
       // We're the last worker for this loop, remove it from the queue.
       t = std::move(std::get<2>(*i));
-      std::shared_ptr<thread_pool::loop> result = std::move(loop);
+      std::shared_ptr<task> result = std::move(loop);
       task_queue_.erase(i);
       return result;
     } else {
@@ -79,7 +79,7 @@ void thread_pool_impl::wait_for(predicate_ref condition, std::condition_variable
 
   std::unique_lock l(mutex_);
   while (!condition()) {
-    loop_task t;
+    task_body t;
     if (auto loop = dequeue(t)) {
       l.unlock();
 
@@ -107,15 +107,15 @@ void thread_pool_impl::wait_for(predicate_ref condition, std::condition_variable
   }
 }
 
-void thread_pool_impl::atomic_call(task_ref t) {
+void thread_pool_impl::atomic_call(function_ref<void()> t) {
   std::unique_lock l(mutex_);
   t();
   cv_worker_.notify_all();
   cv_helper_.notify_all();
 }
 
-std::shared_ptr<thread_pool::loop> thread_pool_impl::enqueue(std::size_t n, loop_task t, int max_workers) {
-  auto loop = std::make_shared<thread_pool::loop>(n);
+std::shared_ptr<task> thread_pool_impl::enqueue(std::size_t n, task_body t, int max_workers) {
+  auto loop = std::make_shared<task_impl>(n);
   std::unique_lock l(mutex_);
   task_queue_.push_back({max_workers, loop, std::move(t)});
   if (n == 1 || max_workers == 1) {
@@ -128,14 +128,14 @@ std::shared_ptr<thread_pool::loop> thread_pool_impl::enqueue(std::size_t n, loop
   return loop;
 }
 
-void thread_pool_impl::run(loop& l, loop_task body) {
+void thread_pool_impl::run(task* l, task_body body) {
   assert(std::find(task_stack.begin(), task_stack.end(), &l) == task_stack.end());
   task_stack.push_back(&l);
-  bool completed = l.run(body);
+  bool completed = l->run(body);
   task_stack.pop_back();
-  if (!completed || !l.done()) {
+  if (!completed || !l->done()) {
     // The loop isn't done, work on other tasks while waiting for it to complete.
-    wait_for([&]() { return l.done(); });
+    wait_for([&]() { return l->done(); });
   }
 }
 
--- base/thread_pool.h
+++ base/thread_pool.h
@@ -14,13 +14,78 @@
 
 namespace slinky {
 
+class task {
+public:
+  // Work on the loop. This returns when work on all items in the loop has started, but may return before all items are
+  // complete. Returns true if this call resulted in the loop being done (any subsequent calls to `run` will do no
+  // work), but the loop may not be done.
+  virtual bool run(function_ref<void(std::size_t)> body) = 0;
+
+  // Returns true if all work is complete.
+  virtual bool done() const = 0;
+};
+
+// This implements a simple thread pool that maps easily to the eval_context thread pool interface.
+// It is not directly used by anything except for testing.
+class thread_pool {
+public:
+  using task_body = std::function<void(std::size_t)>;
+  using predicate_ref = function_ref<bool()>;
+
+  virtual ~thread_pool() = default;
+
+  virtual int thread_count() const = 0;
+
+  // Enqueues a loop task over `n` iterations. The tasks queued in this thread pool are instances of `task`
+  // plus a task body `t` that executes each iteration. Tasks are complete when all `n` of the iterations are done.
+  virtual std::shared_ptr<task> enqueue(
+      std::size_t n, task_body t, int max_workers = std::numeric_limits<int>::max()) = 0;
+  // Run the task on the current thread, and prevents tasks enqueued by `enqueue` from running recursively.
+  // Does not return until the loop is complete.
+  virtual void run(task* l, task_body body) = 0;
+  // Waits for `condition` to become true. While waiting, executes tasks on the queue.
+  // The condition is executed atomically.
+  virtual void wait_for(predicate_ref condition) = 0;
+  // Run `t` on the calling thread, but atomically w.r.t. other `atomic_call` and `wait_for` conditions.
+  virtual void atomic_call(function_ref<void()> t) = 0;
+
+  // Enqueues a singleton task.
+  template <typename Fn>
+  void enqueue(Fn t) {
+    // Make a dummy loop task with one iteration.
+    enqueue(1, [t = std::move(t)](std::size_t) { t(); });
+  }
+
+  template <typename Fn>
+  void parallel_for(std::size_t n, Fn&& body, int max_workers = std::numeric_limits<int>::max()) {
+    if (n == 0) {
+      return;
+    } else if (n == 1) {
+      body(0);
+      return;
+    }
+    max_workers = std::min(max_workers - 1, thread_count());
+    if (max_workers == 0) {
+      // We aren't going to get any worker threads, just run the loop.
+      for (std::size_t i = 0; i < n; ++i) {
+        body(i);
+      }
+    } else {
+      auto loop = enqueue(n, body, max_workers);
+      // Working on the loop here guarantees forward progress on the loop even if no threads in the thread pool are
+      // available.
+      run(loop.get(), std::move(body));
+    }
+  }
+};
+
 constexpr std::size_t cache_line_size = 64;
 
 // This is a helper class for implementing a work stealing scheduler for a parallel for loop. It divides the work among
 // `K` task objects, which can be executed independently by separate threads. When the task is complete, the thread will
 // try to steal work from other tasks.
 template <size_t K = 1>
-class parallel_for {
+class parallel_for final  : public task {
   struct task {
     // i is the next iteration to run.
     alignas(cache_line_size) std::atomic<std::size_t> i;
@@ -60,8 +125,7 @@ public:
 
   // Work on the loop. This returns when work on all items in the loop has started, but may return before all items are
   // complete. Returns true if this call resulted in the loop being done, but the loop may not be done.
-  template <typename Fn>
-  bool run(const Fn& body) {
+  bool run(function_ref<void(std::size_t)> body) override {
     std::size_t w = K == 1 ? 0 : worker_++;
     std::size_t done = 0;
     // The first iteration of this loop runs the work allocated to this worker. Subsequent iterations of this loop are
@@ -92,76 +156,20 @@ public:
     return result;
   }
 
-  bool done() const { return todo_ == 0; }
+  bool done() const override { return todo_ == 0; }
 };
 
 // This implements a simple thread pool that maps easily to the eval_context thread pool interface.
 // It is not directly used by anything except for testing.
-class thread_pool {
-public:
-  using loop = slinky::parallel_for<>;
-  using loop_task = std::function<void(std::size_t)>;
-  using task_ref = function_ref<void()>;
-  using predicate = std::function<bool()>;
-  using predicate_ref = function_ref<bool()>;
-
-  virtual ~thread_pool() = default;
-
-  virtual int thread_count() const = 0;
-
-  // Enqueues a loop task over `n` iterations. The tasks queued in this thread pool are instances of `parallel_for<>`
-  // plus a loop task `t` that executes each iteration. Tasks are complete when all `n` of the iterations are done.
-  virtual std::shared_ptr<loop> enqueue(
-      std::size_t n, loop_task t, int max_workers = std::numeric_limits<int>::max()) = 0;
-  // Run the task on the current thread, and prevents tasks enqueued by `enqueue` from running recursively.
-  // Does not return until the loop is complete.
-  virtual void run(loop& l, loop_task body) = 0;
-  // Waits for `condition` to become true. While waiting, executes tasks on the queue.
-  // The condition is executed atomically.
-  virtual void wait_for(predicate_ref condition) = 0;
-  // Run `t` on the calling thread, but atomically w.r.t. other `atomic_call` and `wait_for` conditions.
-  virtual void atomic_call(task_ref t) = 0;
-
-  // Enqueues a singleton task.
-  template <typename Fn>
-  void enqueue(Fn t) {
-    // Make a dummy loop task with one iteration.
-    enqueue(1, [t = std::move(t)](std::size_t) { t(); });
-  }
-
-  template <typename Fn>
-  void parallel_for(std::size_t n, Fn&& body, int max_workers = std::numeric_limits<int>::max()) {
-    if (n == 0) {
-      return;
-    } else if (n == 1) {
-      body(0);
-      return;
-    }
-    max_workers = std::min(max_workers - 1, thread_count());
-    if (max_workers == 0) {
-      // We aren't going to get any worker threads, just run the loop.
-      for (std::size_t i = 0; i < n; ++i) {
-        body(i);
-      }
-    } else {
-      auto loop = enqueue(n, body, max_workers);
-      // Working on the loop here guarantees forward progress on the loop even if no threads in the thread pool are
-      // available.
-      run(*loop, std::move(body));
-    }
-  }
-};
-
-// This implements a simple thread pool that maps easily to the eval_context thread pool interface.
-// It is not directly used by anything except for testing.
-class thread_pool_impl : public thread_pool {
+class thread_pool_impl final : public thread_pool {
 private:
   int expected_thread_count_ = 0;
   std::atomic<int> worker_count_{0};
   std::vector<std::thread> threads_;
   std::atomic<bool> stop_;
 
-  using queued_task = std::tuple<int, std::shared_ptr<loop>, loop_task>;
+  using task_impl = slinky::parallel_for<>;
+  using queued_task = std::tuple<int, std::shared_ptr<task_impl>, task_body>;
   std::deque<queued_task> task_queue_;
   std::mutex mutex_;
   // We have two condition variables in an attempt to minimize unnecessary thread wakeups:
@@ -174,14 +182,14 @@ private:
 
   void wait_for(predicate_ref condition, std::condition_variable& cv);
 
-  std::shared_ptr<loop> dequeue(loop_task& t);
+  std::shared_ptr<task> dequeue(task_body& t);
 
 public:
   // `workers` indicates how many worker threads the thread pool will have.
   // `init` is a task that is run on each newly created thread.
   // Pass workers = 0 to have a thread pool with no worker threads and
   // use `run_worker` to enter a thread into the thread pool.
-  thread_pool_impl(int workers = 3, task_ref init = nullptr);
+  thread_pool_impl(int workers = 3, function_ref<void()> init = nullptr);
   ~thread_pool_impl() override;
 
   // Enters the calling thread into the thread pool as a worker. Does not return until `condition` returns true.
@@ -192,10 +200,10 @@ public:
 
   int thread_count() const override { return std::max<int>(expected_thread_count_, worker_count_); }
 
-  std::shared_ptr<loop> enqueue(std::size_t n, loop_task t, int max_workers) override;
-  void run(loop& l, loop_task body) override;
+  std::shared_ptr<task> enqueue(std::size_t n, task_body t, int max_workers) override;
+  void run(task* l, task_body body) override;
   void wait_for(predicate_ref condition) override { wait_for(condition, cv_helper_); }
-  void atomic_call(task_ref t) override;
+  void atomic_call(function_ref<void()> t) override;
 };
 
 }  // namespace slinky
