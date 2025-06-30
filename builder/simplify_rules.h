c73283cd58688cf0692bd63de958235b624b0329
Volodymyr Kysenko <vksnk@google.com>
2025-06-30 12:23:03 -0700

    Add a new rule which help with broadcasts in the pyramidal pipelines (#698)

--- builder/simplify_rules.h
+++ builder/simplify_rules.h
@@ -513,6 +513,13 @@ bool apply_less_rules(Fn&& apply) {
         (c0 + 1)*c1 <= x, c1 > 0,
         c0*c1 > x, c1 < 0) ||
 
+      apply((max(x, c0)/may_be<1>(c3)) < max(x, c1)/may_be<1>(c3) + c2,
+        true, c2 > 0 && c3 > 0 && c1/c3 + c2 > c0/c3,
+        false, c2 < 0 && c3 > 0 && c1/c3 + c2 <= c0/c3) ||
+      apply((min(x, c0)/may_be<1>(c3)) < min(x, c1)/may_be<1>(c3) + c2,
+        true, c2 > 0 && c3 > 0 && c1/c3 + c2 > c0/c3,
+        false, c2 < 0 && c3 > 0 && c1/c3 + c2 <= c0/c3) ||
+
       // The following rules are taken from
       // https://github.com/halide/Halide/blob/7636c44acc2954a7c20275618093973da6767359/src/Simplify_LT.cpp#L186-L263
       // with adjustments for the simplifier implementation here.
