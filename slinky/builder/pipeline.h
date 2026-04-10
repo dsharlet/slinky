7c74ea59dc6798335e5987d34dc8c7146db07a69
Volodymyr Kysenko <vksnk@google.com>
2026-04-10 15:26:42 -0700

    Add func::store_innermost to schedule all output buffers to be stored at the innermost loop level

--- slinky/builder/pipeline.h
+++ slinky/builder/pipeline.h
@@ -347,6 +347,13 @@ public:
   bool is_padded_copy() const { return is_padded_copy_; }
 
   stmt make_call() const;
+
+  // Make all output buffer stored at the innermost loop level.
+  void store_innermost() {
+    for (auto& output : outputs_) {
+      output.buffer->store_at({this, slinky::var()});
+    }
+  }
 };
 
 struct build_options {
