f5ef92d1cbcbc783cd0d001470ef38266cb83b3d
Lance Strait <lstrait@google.com>
2026-05-28 18:27:00 +0000

    Add overflow safety to Slinky init_strides

--- slinky/runtime/buffer.h
+++ slinky/runtime/buffer.h
@@ -223,7 +223,7 @@ protected:
 
   std::optional<std::size_t> init_strides_impl(index_t alignment);
 
- public:
+public:
   using element = void;
   using pointer = void*;
 
@@ -424,8 +424,7 @@ protected:
   std::optional<std::size_t> init_strides(index_t alignment = 1) {
     if (rank == 0) {
       std::size_t final_sum;
-      if (add_with_overflow(elem_size, static_cast<std::size_t>(alignment - 1),
-                            final_sum)) {
+      if (add_with_overflow(elem_size, static_cast<std::size_t>(alignment - 1), final_sum)) {
         return std::nullopt;
       }
       return final_sum & ~static_cast<std::size_t>(alignment - 1);
