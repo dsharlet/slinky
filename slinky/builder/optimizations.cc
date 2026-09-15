2389c0dc2bbed17de39bb43012892cd7105a94b2
Dillon <dsharlet@gmail.com>
2026-09-15 09:58:58 -0700

    Avoid redundant evaluation of `let_stmt` at root when possible. (#894)
    This change provides a mechanism to avoid redundantly evaluating the
    `let_stmt` containing all of the constants a pipeline uses every time
    the pipeline is evaluated:
    
    1. We make a separate `let_stmt` containing only constants, at the root
    of the pipeline.
    2. We mark `let_stmt`s that contain only constants as such.
    3. `pipeline::setup` eagerly evaluates these constants.
    4. `pipeline::evaluate` now has a new parameter `is_set_up`, default
    `false`, which if `true`, indicates that the constant lets at root
    should be skipped.

--- slinky/builder/optimizations.cc
+++ slinky/builder/optimizations.cc
@@ -1963,7 +1963,28 @@ stmt optimize_symbols(const stmt& s, node_context& ctx) {
   reuse_shadows mutator;
   result = mutator.mutate(result);
 
-  if (mutator.max_symbol_id >= 0) {
+  if (const let_stmt* let = result.as<let_stmt>()) {
+    int max_symbol_id = std::max(mutator.max_symbol_id, let->max_symbol_id);
+    if (!let->is_constant) {
+      // We can help optimize `pipeline::evaluate` if we separate the constant and non-constant lets.
+      // This is definitely safe because a constant cannot depend on another let.
+      std::vector<std::pair<var, expr>> constants;
+      std::vector<std::pair<var, expr>> non_constants;
+      constants.reserve(let->lets.size());
+      non_constants.reserve(let->lets.size());
+      for (const auto& p : let->lets) {
+        if (p.second.as<constant>() || p.second.as<constant_buffer>()) {
+          constants.push_back(p);
+        } else {
+          non_constants.push_back(p);
+        }
+      }
+      stmt inner = let_stmt::make(std::move(non_constants), let->body, let->is_closure);
+      result = let_stmt::make(std::move(constants), std::move(inner), /*is_closure=*/false, max_symbol_id);
+    } else if (max_symbol_id >= 0 && max_symbol_id != let->max_symbol_id) {
+      result = let_stmt::make(let->lets, let->body, let->is_closure, max_symbol_id);
+    }
+  } else if (mutator.max_symbol_id >= 0) {
     // Some symbols were declared outside of any let_stmt.
     result = let_stmt::make(
         std::vector<std::pair<var, expr>>{}, std::move(result), /*is_closure=*/false, mutator.max_symbol_id);
